e0ea21a1f021400d563d724a6dbe9dcd7e39afbd
Madelline Butcher <madellinebutcher@gmail.com>
2018-05-27 12:24:11 -0600

    updates

--- www/src/components/Home.vue
+++ www/src/components/Home.vue
@@ -1,119 +1,84 @@
 <template>
-  <div class="home">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="hello">
+
+    <div class="searchbar">
+      <form @submit.prevent="findSongs">
+        <input type="text" v-model="query">
+      </form>
+    </div>
+    <div class="songs-section">
+
+      <div class="results">
+        <h4>Results for {{title}}:</h4>
+        <div class="row">
+          <songs :list="songs" button-text="Add to playlist" :handle-button-click="addSong"></songs>>
+        </div>
+      </div>
+      <hr>
+      <div class="playlist">
+        <div class="row">
+          <songs :list="playlist" button-text="Remove from playlist" :handle-button-click="removeSong"></songs>
+        </div>
+      </div>
+    </div>
+
+    <div class="detailed-view">
+      details here
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import Playlist from './Playlist.vue'
+  export default {
+    name: 'Home',
+    components: {
+      songs
+    },
+    data() {
+      return {
+        query: '',
+        title: ''
+      }
+    },
+    computed: {
+      songs() {
+        return this.$store.state.songs
+      },
+      playlist() {
+        return this.$store.state.playlist
+      },
+      activeSong() {
+        return this.$store.state.activeSong
+      }
+    },
+    methods: {
+      findSongs() {
+        this.$store.dispatch('findSongs', this.query)
+        this.title = this.query
+        this.query = ''
+      },
+      addSongToPlaylist(song) {
+        this.$store.dispatch('addSong', song)
+      },
+      removeSongFromPlaylist(song) {
+        this.$store.dispatch('removeSong', song)
+      }
     }
-  },
-  computed: {
-
-  },
-  methods: {
-    
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .songs-section {
+    display: grid;
+    grid-template-areas: "results playlist"
+  }
+  .results {
+    grid-area: results
+  }
+  .playlist {
+    grid-area: watchlist
+  }
 </style>
--- www/src/store/mytunes-service.js
+++ www/src/store/mytunes-service.js
@@ -2,7 +2,7 @@ import vue from 'vue'
 import vuex from 'vuex'
 import axios from 'axios'
 import router from '../router'
-import store from '../store'
+// import store from '../store'
 
 vue.use(vuex)
 
@@ -15,16 +15,33 @@ export default new vuex.Store({
   state:{
     songs: [],
     playlist: [],
+    activeSong: {}
   },
   mutations:{
     addSongToPlaylist(state, song){
       state.playlist.push(song)
+    },
+    removeSongFromPlaylist(state, indexToRemove){
+      state.playlist.splice(indexToRemove, 1)
+    },
+    setSongs(state, songs){
+      state.songs = songs
+    },
+    setActiveSong(state, song){
+      state.activeSong = song
     }
+
   },
   actions:{
-    addSongToPlaylist({commit, dispatch}, song){
+    addSongToPlaylist({commit, dispatch, state}, song){
       commit('addSongToPlaylist', song)
       router.push({name: 'song'})
+    },
+    findSongs({commit, dispatch}, query){
+      api.get(query)
+        .then(res=> {
+          commit('setSongs', res.data.results)
+        })
     },
     getTracks() { },
     addTrack() { },
